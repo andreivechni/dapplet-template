96083de61d10887a092aafe92d95269aedeac9f6
Nikita Teremovskiy <teremovskiin@gmail.com>
2021-02-04 18:22:45 +0300

    feat: solution for the example 13.

--- overlay/src/index.tsx
+++ overlay/src/index.tsx
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 // LP:  7. Change by adding Share State HOC
-import App from './App';
+import App, { IState } from './App';
+import { dappletState } from '@dapplets/dapplet-overlay-bridge';
+
+const DappletState = dappletState<IState>(App);
 
 ReactDOM.render(
-  <React.StrictMode><App/></React.StrictMode>,
+  <React.StrictMode><DappletState/></React.StrictMode>,
   document.getElementById('root'),
 );
 // LP end
--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,10 @@ import { } from '@dapplets/dapplet-extension';
 import EXAMPLE_IMG from './icons/ex13-19px.png';
 
 // LP:  1. Implement an interface of the state with a counter and a text
-
+interface IState {
+  counter: number
+  text: string
+}
 // LP end
 
 @Injectable
@@ -12,13 +15,14 @@ export default class TwitterFeature {
 
   activate() {
     // LP:  2. Create a shared state
-    
+    const state = Core.state<IState>({ counter: 0, text: '' });
     // LP end
     // LP:  3. Then create an overlay typing with `IState` interface
-    
+    const overlay = Core.overlay<IState>({ name: 'example-13-overlay', title: 'Example 13' })
+      .useState(state);
     // LP end
     // LP:  4. Add home button with the overlay opening
-    
+    Core.onAction(() => overlay.open());
     // LP end
 
     const { button, input } = this.adapter.exports;
@@ -28,11 +32,13 @@ export default class TwitterFeature {
           DEFAULT: {
             img: EXAMPLE_IMG,
             // LP:  5.  Pass state's counter to the label parameter
-            
+            label: state[ctx.id].counter,
             // LP end
             exec: () => {
               // LP:  6. Increse the counter and open the overlay
-              
+              const oldValue = state[ctx.id].counter.value;
+              state[ctx.id].counter.next(oldValue + 1);
+              overlay.open(ctx.id);
               // LP end
             },
           },
@@ -40,7 +46,7 @@ export default class TwitterFeature {
         input({
           DEFAULT: {
             // LP:  5.  Pass state's text to the text parameter
-            
+            text: state[ctx.id].text
             // LP end
           }
         })
