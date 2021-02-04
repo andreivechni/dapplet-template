a035eae10a26ddc1525500a39aadad58fb84424a
Nikita Teremovskiy <teremovskiin@gmail.com>
2021-02-04 18:22:45 +0300

    feat: prepare template for the example 13.

---
+++ overlay/src/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+// LP:  7. Change by adding Share State HOC
+import App from './App';
+
+ReactDOM.render(
+  <React.StrictMode><App/></React.StrictMode>,
+  document.getElementById('root'),
+);
+// LP end
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
 import { } from '@dapplets/dapplet-extension';
-import EXAMPLE_IMG from './icons/dapplet-icon.png';
+import EXAMPLE_IMG from './icons/ex13-19px.png';
+
+// LP:  1. Implement an interface of the state with a counter and a text
+
+// LP end
 
 @Injectable
 export default class TwitterFeature {
@@ -7,17 +11,40 @@ export default class TwitterFeature {
   @Inject('twitter-adapter.dapplet-base.eth') public adapter: any;
 
   activate() {
-    const { button } = this.adapter.exports;
+    // LP:  2. Create a shared state
+    
+    // LP end
+    // LP:  3. Then create an overlay typing with `IState` interface
+    
+    // LP end
+    // LP:  4. Add home button with the overlay opening
+    
+    // LP end
+
+    const { button, input } = this.adapter.exports;
     this.adapter.attachConfig({
-      POST: () =>
+      POST: (ctx: any) => ([
         button({
-          initial: 'DEFAULT',
           DEFAULT: {
-            label: 'Injected Button',
             img: EXAMPLE_IMG,
-            exec: () => alert('Hello, World!'),
+            // LP:  5.  Pass state's counter to the label parameter
+            
+            // LP end
+            exec: () => {
+              // LP:  6. Increse the counter and open the overlay
+              
+              // LP end
+            },
           },
         }),
+        input({
+          DEFAULT: {
+            // LP:  5.  Pass state's text to the text parameter
+            
+            // LP end
+          }
+        })
+      ])
     });
   }
 }
