ed4ff9cf4f01bd3cb84466a2d6df5885c57cd6c2
Nikita Teremovskiy <teremovskiin@gmail.com>
2021-03-01 18:23:06 +0300

    feat: prepare template for the example 3.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import {} from '@dapplets/dapplet-extension';
-import EXAMPLE_IMG from './icons/dapplet-icon.png';
+import EXAMPLE_IMG from './icons/ex03.png';
+//import STAMP_IMG from './icons/fakeStamp.png';
 
 @Injectable
 export default class TwitterFeature {
@@ -7,17 +8,28 @@ export default class TwitterFeature {
   @Inject('twitter-adapter.dapplet-base.eth') public adapter: any;
   
   activate() {
+    // LP: 1. Get the widget 'picture' from adapter
     const { button } = this.adapter.exports;
-    this.adapter.attachConfig({
-      POST: () =>
+    // LP end
+    // $ returns object "me". Use it to change state or params of the other widget
+    // Example 1: exec: () => $(ctx, 'another_el_id').state = 'SECOND'
+    // Example 2: exec: () => $(ctx, 'another_el_id').label = 'Hello'
+    const { $ } = this.adapter.attachConfig({
+      POST: (ctx) => [
         button({
-          initial: 'DEFAULT',
+          id: 'button',
           DEFAULT: {
             label: 'Injected Button',
             img: EXAMPLE_IMG,
-            exec: () => alert('Hello, World!'),
+            // LP: 2. Toggle the state “hidden/shown” of the picture on button click
+
+            // LP end
           },
         }),
+        // LP: 1. Add extra picture and make it hidden by default
+
+        // LP end
+      ],
     });
   }
 }
